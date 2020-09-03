40c172cf033369ab68f9735d49c58d4cffb326ce
Jacob Mansfield <jacob.mansfield@itvet.co.uk>
2020-09-03 16:44:19 +0100

    PowerShell script to count number of files and stencils

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ PRs welcome for new shapes/stencils and fixes to existing ones (e.g. adding conn
 This repo contains:
 - 526 Visio 2003-2010 Stencil Files (.vss)  
 - 105 Visio 2016+ Stencil Files (.vssx)  
-- 27,974 Stencils total  
+- 27,650 Stencils total  
 Last updated 2020-09-03
 
 ## Instructions
---
+++ generateStats.ps1
@@ -0,0 +1,36 @@
+
+function Show-ProgressV3 {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true, Position=0, ValueFromPipeline=$true)]
+        [PSObject[]]$InputObject,
+        [string]$Activity = "Processing items"
+    )
+
+        [int]$TotItems = $Input.Count
+        [int]$Count = 0
+
+        $Input|foreach {
+            $_
+            $Count++
+            [int]$percentComplete = ($Count/$TotItems* 100)
+            Write-Progress -Activity $Activity -PercentComplete $percentComplete -Status ("Working - " + $percentComplete + "%") -CurrentOperation (""+$Count+"/"+$TotItems+" - "+$_.Name)
+        }
+}
+
+$count = 0
+$Visio = New-Object -ComObject Visio.Application
+
+(Get-ChildItem -Recurse -Include @("*.vss", "*.vssx")) | Show-ProgressV3 | Foreach-Object {
+    $doc = $Visio.Documents.OpenEx($_.FullName, 192)
+    $count += $doc.Masters.Count
+    $doc.close()
+
+    Start-Sleep 1
+}
+
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($Visio) | Out-Null
+
+Write-Host "Old template files:" (Get-ChildItem -Recurse -Include "*.vss").Count
+Write-Host "New template files:" (Get-ChildItem -Recurse -Include "*.vssx").Count
+Write-Host "Total visio stencils:" $count
